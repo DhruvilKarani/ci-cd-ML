70d116d7800c2bc595f2ecf0ae7cc142c93390a7
Dhruvil Karani <karanidhruvil@gmail.com>
2024-03-02 21:22:33 +0530

    fix path

--- train.py
+++ train.py
@@ -3,28 +3,32 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import json
-
+import os
 # Load MNIST dataset
-X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+# X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
 
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# # Split the dataset into training and testing sets
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create a logistic regression model
-model = LogisticRegression()
+# # Create a logistic regression model
+# model = LogisticRegression()
 
-# Train the model
-model.fit(X_train, y_train)
+# # Train the model
+# model.fit(X_train, y_train)
 
-# Make predictions on the test set
-y_pred = model.predict(X_test)
+# # Make predictions on the test set
+# y_pred = model.predict(X_test)
 
-# Calculate accuracy
-accuracy = accuracy_score(y_test, y_pred)
+# # Calculate accuracy
+# accuracy = accuracy_score(y_test, y_pred)
+accuracy = 0.9
 print(f"accuracy={accuracy}")
 
+#join current dir with 'metrics/metrics.json' to get the path
+path = os.path.join(os.getcwd(), 'metrics/metrics.json')
+
 # write metrics to json in root directory
-with open('metrics/metrics.json', 'w') as outfile:
+with open(path, 'w') as outfile:
     json.dump({"accuracy": accuracy}, outfile)
 
 
